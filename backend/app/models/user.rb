7399961486cb228219b562c1ca3a7d0009b9c1d3
keithburgie <krburgie@gmail.com>
2019-07-10 12:29:08 -0400

    Remove the serializers

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # has_many :messages, dependent: :destroy
+  has_many :messages, dependent: :destroy
   #belongs_to :conversation
 end
--- backend/app/serializers/conversation_serializer.rb
+++
@@ -1,5 +0,0 @@
-class ConversationSerializer < ActiveModel::Serializer
-  attributes :id, :name
-  has_many :messages
-  has_many :users, through: :messages
-end
